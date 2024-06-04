325259ff86ac9fba155a712a19ff4c8cb84ba66b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-06-04 14:44:09 +0200

    feat: Add GDPR manage consent link

---
+++ src/components/GDPRConsentLink.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const gdpr = ref(false);
+
+onMounted(() => {
+    if (window['nitroAds'] && window['nitroAds'].loaded) {
+        gdpr.value = window['__tcfapi'] ? true : false;
+    } else {
+        document.addEventListener('nitroAds.loaded', () => {
+            gdpr.value = window['__tcfapi'] ? true : false;
+        });
+    }
+});
+</script>
+
+<template>
+    <div v-show="gdpr" id="ncmp-consent-link"></div>
+</template>
