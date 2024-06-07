06f85f1d4a1ee1bb47bb5e5d1845a17bf05e03d4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-06-07 10:27:45 +0200

    download page but still prerendered

--- src/components/DownloadsFilter.vue
+++ src/components/DownloadsFilter.vue
@@ -1,29 +1,43 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 const props = defineProps<{
     entry: {
         data: {
+            modId: string;
             gameVersions: string[];
             loaders: string[];
         };
     };
+    game: string;
+    gameVersion: string;
+    loader: string;
+    branch: string;
 }>();
+
+const gameVersion = ref(props.gameVersion);
+const loader = ref(props.loader);
+
+const updateUrl = () => {
+    window.location.href = `/${props.game}/${props.entry.data.modId}/download/${gameVersion.value}/${loader.value.toLowerCase()}/${props.branch}`;
+};
 </script>
 
 <template>
     <label class="form-control w-full max-w-xs">
-            <div class="label">
-                <span class="label-text">Game Version</span>
-            </div>
-            <select class="select select-bordered">
-                {entry.data.gameVersions.map((gameVersion) => <option>{gameVersion}</option>)}
-            </select>
-        </label>
-        <label class="form-control w-full max-w-xs">
-            <div class="label">
-                <span class="label-text">Mod Loader</span>
-            </div>
-            <select class="select select-bordered">
-                {entry.data.loaders.map((loader) => <option>{loader}</option>)}
-            </select>
-        </label>
+        <div class="label">
+            <span class="label-text">Game Version</span>
+        </div>
+        <select class="select select-bordered" v-model="gameVersion" @change="updateUrl">
+            <option v-for="gameVersion in entry.data.gameVersions" :key="gameVersion">{{ gameVersion }}</option>
+        </select>
+    </label>
+    <label class="form-control w-full max-w-xs">
+        <div class="label">
+            <span class="label-text">Mod Loader</span>
+        </div>
+        <select class="select select-bordered" v-model="loader" @change="updateUrl">
+            <option v-for="loader in entry.data.loaders" :key="loader">{{ loader }}</option>
+        </select>
+    </label>
 </template>
