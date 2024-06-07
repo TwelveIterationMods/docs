1a2d3b68ce2a077bcca066a9da49d184a9964256
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-06-07 10:15:58 +0200

    fix last commit

---
+++ src/components/DownloadsFilter.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{
+    entry: {
+        data: {
+            gameVersions: string[];
+            loaders: string[];
+        };
+    };
+}>();
+</script>
+
+<template>
+    <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">Game Version</span>
+            </div>
+            <select class="select select-bordered">
+                {entry.data.gameVersions.map((gameVersion) => <option>{gameVersion}</option>)}
+            </select>
+        </label>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">Mod Loader</span>
+            </div>
+            <select class="select select-bordered">
+                {entry.data.loaders.map((loader) => <option>{loader}</option>)}
+            </select>
+        </label>
+</template>
--- src/pages/api/files.ts
+++ src/pages/api/files.ts
@@ -1,28 +1,30 @@
 import type { APIRoute } from 'astro';
-import searchMaven from '../../utils/search-maven';
+import listModFiles from '../../utils/list-mod-files';
 
 export const GET: APIRoute = async ({ request }) => {
     const url = new URL(request.url);
     const query = url.searchParams;
-    const branch = query.get('branch');
-    const repository = branch === 'snapshot' ? 'maven-snapshots' : 'maven-releases';
-    const modId = query.get('modId');
-    const loader = query.get('loader');
+    const modId = query.get('mod');
+    if (!modId) {
+        return new Response('Missing mod parameter', {
+            status: 400,
+        });
+    }
     const gameVersion = query.get('gameVersion');
-    const versions = await searchMaven(repository, 'net.blay09.mods', modId + '-' + loader);
-    const jars = versions.map((version) => {
-        const jarAsset = version.assets.find(
-            (asset) => asset.contentType == 'application/java-archive' && asset.maven2.extension === 'jar' && !asset.maven2.classifier
-        );
-        return {
-            version: version.version,
-            downloadUrl: jarAsset?.downloadUrl.replace('maven-snapshots', 'maven-public').replace('maven-releases', 'maven-public') ?? '',
-            fileSize: jarAsset?.fileSize ?? 0,
-            lastModified: jarAsset?.lastModified,
-        };
-    });
-    const filteredJars = jars.filter((jar) => jar?.version.endsWith('+' + gameVersion));
-    return new Response(JSON.stringify(filteredJars), {
+    if(!gameVersion) {
+        return new Response('Missing gameVersion parameter', {
+            status: 400,
+        });
+    }
+    const loader = query.get('loader');
+    if (!loader) {
+        return new Response('Missing loader parameter', {
+            status: 400,
+        });
+    }
+    const branch = query.get('branch') ?? 'release';
+    const files = listModFiles(modId, gameVersion, loader, branch);
+    return new Response(JSON.stringify(files), {
         headers: {
             'content-type': 'application/json',
         },
